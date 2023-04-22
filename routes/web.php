591b43ee049692d40be188f5d1a2245b1a1b8ffe
Artur Kustra <artur.kustra@gmail.com>
2023-04-22 16:02:45 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return response()->json(['ok']);
+});
+
+Route::get('/ping', function () {
+    return response()->json([
+        "data" => [
+            "date" => Carbon::now()->format('Y-m-s H:i:s')
+        ]
+    ]);
+});
+
+require __DIR__ . '/auth.php';
